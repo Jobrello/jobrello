ae494c9c8ba7e33a10d18d66084bfcf4c7bc8684
Marcin Golenia <marcin.g@xml-int.com>
2022-05-21 22:49:57 +0200

    svg changes + dumb svelte slider

---
+++ src/components/Slider.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    export let value: number = 1
+    export let min: number = 0
+    export let max: number = 5
+
+</script>
+
+<style>
+.slider {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 15px;
+  border-radius: 5px;  
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%; 
+  background: #04AA6D;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #04AA6D;
+  cursor: pointer;
+}
+</style>
+
+<div class="slidecontainer">
+    <input bind:value={value} type="range" min={min} max={max} class="slider">
+</div>
