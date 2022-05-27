41e52e05102140312add837b3519e080fe120885
Marcin Golenia <marcin.g@xml-int.com>
2022-05-27 10:11:53 +0200

    file zone

---
+++ src/components/Upload.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  export let dropFileTitle = "Drop file here"
+
+  let isOver = false
+
+  const handleDrop = (ev: DragEvent) => {
+    ev.preventDefault()
+    console.log(ev)
+    isOver = false
+  }
+
+  const onOver = (ev: DragEvent) => {
+    ev.preventDefault()
+    isOver = true
+  }
+
+</script>
+
+<style>
+  .dropzone {
+    width:200px; 
+    height:75px; 
+    border:1px dashed black; 
+    border-radius:.25rem; 
+    text-align:center;
+  }
+
+  .isOver {
+    background-color: burlywood;
+  }
+</style>
+
+<div
+  on:dragover={onOver} 
+  on:dragleave={() => isOver = false}
+  on:drop={handleDrop} 
+  class="dropzone"
+  class:isOver>
+  {dropFileTitle}
+  <input type="file" id="myFile" name="filename">
+</div>
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,11 +1,19 @@
 import '@testing-library/jest-dom'
-import Counter from '../src/components/Counter.svelte'
-import { render } from '@testing-library/svelte'
+import Upload from '../src/components/Upload.svelte'
+import {render, fireEvent} from '@testing-library/svelte'
 
-describe('ddd', () => {
-  it(`test`, () => {
-    const cut = render(Counter)
-    // Arrange + Act
-    expect(1).toBe(1)
+describe('Upload component', () => {
+  it(`When file is dropped, then it can be found in the input`, async () => {
+    const testFile = new File(['(⌐□_□)'], 'chucknorris.png', {
+      type: 'image/png'
+    })
+    const dropFileName = 'Drop job offer here'
+    const cut = render(Upload, {dropFileName: 'Drop job offer here'})
+    // Act
+    fireEvent.drop(cut.getByLabelText(dropFileName), {
+      dataTransfer: {files: [testFile]}
+    })
+    // Assert
+    expect(await cut.findByText(testFile.name)).toBeInTheDocument()
   })
 })
